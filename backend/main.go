43b4ca1277d2bfa8322c2279777f7a6d53a1605d
Thorik <2231730055@student.polinema.ac.id>
2026-06-05 16:03:25 +0700

    fix

--- backend/main.go
+++ backend/main.go
@@ -11,7 +11,7 @@ import (
 
 func enableCors(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
 
